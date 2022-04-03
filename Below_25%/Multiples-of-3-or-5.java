99540007d7612caaf80136e6c9313cbbf9c97e25
Benjamin-Humburg <102706500+Benjamin-Humburg@users.noreply.github.com>
2022-04-02 18:01:22 -0700

    Update Multiples-of-3-or-5.java

--- Below_25%/Multiples-of-3-or-5.java
+++ Below_25%/Multiples-of-3-or-5.java
@@ -7,3 +7,36 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 */
 
+package ProjectEuler;
+
+public class Solutions {
+	
+	public static void main(String[] args) {
+		
+		
+		final int multiple1 = 3;
+		final int multiple2 = 5;
+		final int belowThis = 1000;
+		
+		System.out.println("Solution is: " + sumOfAUB(3, 5, 1000));
+		
+	}
+	
+	public static int sumOfAUB(int multiple1, int multiple2, int belowThis) {
+		
+		int numMult1 = (int) (Math.ceil((double) belowThis / (double) multiple1) - 1);
+		int numMult2 = (int) (Math.ceil((double) belowThis / (double) multiple2) - 1);
+		int numMultIntersection = (int) (Math.ceil((double) belowThis / ((double) multiple1 * (double) multiple2)) - 1);
+		
+		int sumOfMults1 = multiple1 * sumOfFirstNNaturals(numMult1);
+		int sumOfMults2 = multiple2 * sumOfFirstNNaturals(numMult2);
+		int sumOfInters = (multiple1 * multiple2) * sumOfFirstNNaturals(numMultIntersection);
+		
+		return sumOfMults1 + sumOfMults2 - sumOfInters;
+	}
+	
+	public static int sumOfFirstNNaturals(int n) {
+		
+		return (n * (n + 1)) / 2;
+	}
+}
